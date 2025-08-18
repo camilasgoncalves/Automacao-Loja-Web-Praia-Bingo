6b02f5acc61feeeb5cf4f021f96b0ee0e8105063
Camila Gonçalves <c.goncalves@pipastudios.com>
2025-08-18 15:13:52 -0300

    Teste workflow

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   reporter: "cypress-mochawesome-reporter",
   reporterOptions: {
-    reportDir: "cypress/reports/mochawesome",
+    reportDir: "cypress/reports",
     overwrite: false,
     html: true,
     json: true,
