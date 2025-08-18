0e2f031e502b69468d547259ebe56f4b18796f11
Camila Gonçalves <c.goncalves@pipastudios.com>
2025-08-18 13:33:20 -0300

    Teste Workflow

--- cypress.config.js
+++ cypress.config.js
@@ -1,29 +1,19 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter', 
+  reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
     reportDir: 'cypress/reports/mochawesome',
-    overwrite: false,
+    overwrite: true,
     html: true,
     json: true,
     embeddedScreenshots: true,
-    inlineAssets: true,
+    inlineAssets: true
   },
   e2e: {
-    responseTimeout: 15000,
-    defaultCommandTimeout: 15000,
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
-
-      on('task', {
-        log(message) {
-          console.log(message);
-          return null;
-        },
-      });
-
       return config;
-    },
-  },
+    }
+  }
 });
