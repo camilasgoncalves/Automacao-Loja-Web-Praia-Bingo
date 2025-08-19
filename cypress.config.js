94de80704998b0098d7c85dcfec8d2f00420dcb7
Camila Gonçalves <c.goncalves@pipastudios.com>
2025-08-19 09:08:38 -0300

    Mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,23 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    reportFileName: 'mochawesome',
+    overwrite: false,
+    html: true,
+    json: false
+  },
+
   e2e: {
     responseTimeout: 15000,
     defaultCommandTimeout: 15000,
     setupNodeEvents(on, config) {
      
+      require('cypress-mochawesome-reporter/plugin')(on)
+
       on("task", {
         log(message) {
           console.log(message)
