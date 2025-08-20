cb16f80ed8def6c2e08951b95e869a1795dae549
Camila Gonçalves <c.goncalves@pipastudios.com>
2025-08-20 07:34:01 -0300

    Teste

--- cypress/PO/homePO.js
+++ cypress/PO/homePO.js
@@ -81,7 +81,7 @@ export class Home {
         cy.wait(5000)
         cy.get('button').invoke('text').then($el => {
             $el.includes('Aceitar tudo') ? this.elementos.btnAceitarTudo().click({ force: true }) : undefined
-            $el.includes('Dispensar') ? cy.get('#reject-button').should('be.visible').click({ force: true }) : undefined
+            $el.includes('Dispensar') ? this.elementos.btnDispensar().click({ force: true }) : undefined
         })
     }
 
